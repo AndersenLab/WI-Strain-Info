2d204b8bd713a014d5dea070f745db77c361dd9e
Daniel Cook <danielecook@gmail.com>
2018-02-14 11:26:37 -0600

    Cleanup

--- process.sh
+++ process.sh
@@ -10,11 +10,7 @@ setup_git() {
 
 commit_files() {
     git checkout -qf master
-    ls *
     wget -O WI-Strain-Info.tsv https://docs.google.com/spreadsheets/d/1V6YHzblaDph01sFDI8YK_fP0H7sVebHQTXypGdiQIjI/export?format=tsv&id=1V6YHzblaDph01sFDI8YK_fP0H7sVebHQTXypGdiQIjI&gid=0
-    ls *
-    pwd
-    ls -lah
     git add WI-Strain-Info.tsv
     git remote add origin https://${GH_TOKEN}@github.com/WI-Strain-Info/resources.git > /dev/null 2>&1
     git commit --message "Travis build: ${TRAVIS_BUILD_NUMBER}"
