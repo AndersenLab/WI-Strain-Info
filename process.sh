d4b2a59e6d0ad6cbf2b042b81ab3c4efc4a112c4
Daniel Cook <danielecook@gmail.com>
2018-02-14 10:43:43 -0600

    Update process

--- process.sh
+++ process.sh
@@ -3,20 +3,17 @@
 setup_git() {
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
+  git config --global github.token ${GH_TOKEN}
 }
 
-commit_website_files() {
+commit_files() {
     curl https://docs.google.com/spreadsheets/d/1V6YHzblaDph01sFDI8YK_fP0H7sVebHQTXypGdiQIjI/export?format=tsv&id=1V6YHzblaDph01sFDI8YK_fP0H7sVebHQTXypGdiQIjI&gid=0 > WI-Strain-Info.tsv
-    git checkout -b gh-pages
     git add WI-Strain-Info.tsv
-    git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
-}
-
-upload_files() {
-  git remote add origin-pages https://${GH_TOKEN}@github.com/MVSE-outreach/resources.git > /dev/null 2>&1
-  git push --quiet --set-upstream origin-pages gh-pages 
+    git commit --message "Travis build: ${TRAVIS_BUILD_NUMBER}"
+    if [[ $? -eq 0 ]]; then
+        git push --quiet
+    fi;
 }
 
 setup_git
-commit_website_files
-upload_files
+commit_files
